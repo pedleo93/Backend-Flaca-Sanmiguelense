0654562624a971130fb37c801b70c7f72ba3fd1f
Tomas Alvarez <soytalvarez17@gmail.com>
2024-10-07 20:27:54 -0600

    Primera Carga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
-  {path: 'principal', loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)},
   {path: '**',redirectTo: 'login'}
 ]
 
